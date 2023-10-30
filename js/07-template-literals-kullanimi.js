8e0b1ad70d31f826d3a6f064333ffb288858255a
Gulsumj <glsmkaymazz@gmail.com>
2023-10-30 21:48:01 +0300

    day08

--- js/07-template-literals-kullanimi.js
+++ js/07-template-literals-kullanimi.js
@@ -1,6 +1,22 @@
-let userName = "gulsum"
+// ********** Template Literals Kullanimi ********** 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+let username = "hakan"
 const DOMAIN = "kodluyoruz.org"
 
-let email = userName + "@" + DOMAIN
+let email = username + "@" + DOMAIN
 
-console.log("Merhaba", userName, "sitemize hoşgeldin", "mail adresin:", email)
+// console.log("Merhaba", username, "sitemize hosgeldin", "mail adresin:", email)
+
+let info = `
+Merhaba ${username} sitemize hosgeldin.. 
+mail adresin: ${email}
+
+mail adresinin uzunlugu: ${email.length}
+borcunuz: ${(2 + 3) * 10} TL
+gunun saat bilgisi : ${new Date().getHours()}
+
+kisa isminiz: ${username[0]}.
+`
+
+console.log(info)
